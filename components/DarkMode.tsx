a70282e08f5d4d6c0008a107bdff033fe4ba1aaf
Md Jubayer <mdjubayerislamshuvo34@gmail.com>
2025-09-03 23:04:35 +0600

    fgty

--- components/DarkMode.tsx
+++ components/DarkMode.tsx
@@ -1,3 +1,4 @@
+'use client';
 import React, { useState, useEffect } from 'react';
 import { SunIcon, MoonIcon } from '@heroicons/react/24/solid';
 
--- components/LogoutBtn.tsx
+++ components/LogoutBtn.tsx
@@ -1,3 +1,4 @@
+'use client';
 import React from "react";
 import { useDispatch } from "react-redux";
 import { adminLogout } from "@/lib/adminSlice";
